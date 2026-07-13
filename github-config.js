8c0a4dcbbbce9d64f282a709ae3ef8b576678b2e
andrewcsa <andrewchai@outlook.com>
2026-07-13 21:14:57 +1200

    Create github-config.js

---
+++ github-config.js
@@ -0,0 +1,3 @@
+const GITHUB_CONFIG = { endpoint: 'https://api.github.com/repos/andrewcsa/PB_App/contents/PB_Playerlist.csv',
+                       branch: 'main',
+                       token: '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' };
